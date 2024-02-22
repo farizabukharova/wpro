f6ad692fefc9def63101b14efb0653559a8bf7f8
Fariza <fariza.bukharova.@narxoz.kz>
2024-02-22 17:28:08 +0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//       publicPath: '/',
+//     },
+//     open: true
+//   }
+// };
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ],
+    devServer: {
+        compress: true,
+        port: 8080,
+        historyApiFallback: true,
+    },
+};
+module.exports = {
+    mode: 'development',
+};
